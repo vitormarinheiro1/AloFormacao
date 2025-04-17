907ec1b353c312de9486128fd62a58e80103e10b
Vitor Marinheiro Rodrigues <x396751@prodam.pref>
2025-04-17 14:04:56 -0300

    add autenticacao jwt

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -127,7 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication'
+        # 'rest_framework.authentication.BasicAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
     'rest_framework.permissions.IsAuthenticated',

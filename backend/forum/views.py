a630ec8300e3a5f25c477fc77e2868fdda9a2b89
Vitor Marinheiro Rodrigues <x396751@prodam.pref>
2025-04-17 10:16:39 -0300

    add filtros de ordenação

--- backend/forum/views.py
+++ backend/forum/views.py
@@ -1,12 +1,15 @@
 from forum.models import Aluno, Curso, Reclamacao, Comentario
 from forum.serializers import AlunoSerializer, CursoSerializer, ReclamacaoSerializer, ComentarioSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 class AlunoViewSet(viewsets.ModelViewSet):
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_filters = ['nome']
 
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()

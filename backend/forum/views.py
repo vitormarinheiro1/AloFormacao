ee1dba770f0b969cd2a6e08b10193be00fbfda62
Vitor Marinheiro Rodrigues <x396751@prodam.pref>
2025-04-17 11:03:35 -0300

    limitando acessos da api por usuarios logados e anonimos

--- backend/forum/views.py
+++ backend/forum/views.py
@@ -5,22 +5,26 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
+
 class AlunoViewSet(viewsets.ModelViewSet):
-    queryset = Aluno.objects.all()
+    queryset = Aluno.objects.all().order_by("id")
     serializer_class = AlunoSerializer
 
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_filters = ['nome']
     search_fields = ['nome', 'email']
 
+
 class CursoViewSet(viewsets.ModelViewSet):
-    queryset = Curso.objects.all()
+    queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
 
+
 class ReclamacaoViewSet(viewsets.ModelViewSet):
-    queryset = Reclamacao.objects.all()
+    queryset = Reclamacao.objects.all().order_by("id")
     serializer_class = ReclamacaoSerializer
 
+
 class ComentarioViewSet(viewsets.ModelViewSet):
-    queryset = Comentario.objects.all()
+    queryset = Comentario.objects.all().order_by("id")
     serializer_class = ComentarioSerializer
--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -132,5 +132,13 @@ REST_FRAMEWORK = {
     'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20,
+        'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/day',
+        'user': '50/day'
+    }
 }

beb60bf25e9f33eb5bd75f647d66098b442f06c6
Vitor Marinheiro Rodrigues <x396751@prodam.pref>
2025-04-17 13:10:24 -0300

    add documentacao swagger

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'forum',
     'rest_framework',
-    'django_filters'
+    'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- backend/setup/urls.py
+++ backend/setup/urls.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from forum.views import AlunoViewSet, CursoViewSet, ReclamacaoViewSet, ComentarioViewSet
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Documentação da API",
+      default_version='v1',
+      description="Documentação da API AloFormacao",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+)
 
 router = routers.DefaultRouter()
 router.register('alunos', AlunoViewSet, basename='Alunos')
@@ -12,5 +27,7 @@ router.register('comentario', ComentarioViewSet, basename='Comentario')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]

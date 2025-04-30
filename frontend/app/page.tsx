f84e18a095bab36834825973f7a79cf8aaa5ab59
Vitor Marinheiro Rodrigues <x396751@prodam.pref>
2025-04-30 08:34:40 -0300

    add shadcn in frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,14 @@
+import { Input } from "@/components/ui/input"
+
 export default function Home() {
   return (
     <>
       <header>
         <h1 className="text-blue-800 text-5xl font-bold ml-12 mt-12">AloFormação</h1>
       </header>
+      <div>
+        <Input type="email" placeholder="Email" />
+      </div>
     </>
   );
 }
